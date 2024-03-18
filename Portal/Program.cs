391f634c036200b3eb9384d4d24c6abf9aca39a0
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2024-03-18 17:40:19 +0100

    Created portal with net program.cs

---
+++ Portal/Program.cs
@@ -0,0 +1,15 @@
+﻿using DomainServices;
+using Infrastructure;
+
+namespace Portal
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeFactory factory = new EmployeeFactory();
+            IEmployee testdeveloper = factory.CreateEmployee("Developer");
+            IEmployee testproductowner = factory.CreateEmployee("ProductOwner");
+        }
+    }
+}
