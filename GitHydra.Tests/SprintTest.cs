dc020200e174e86fc1e04de7e001dacde853a87a
Rensvdk20 <rensvdk@gmail.com>
2024-03-19 09:53:51 +0100

    Added sprint export tests

---
+++ GitHydra.Tests/SprintTest.cs
@@ -0,0 +1,38 @@
+﻿using Domain;
+using Domain.Employees;
+using Infrastructure.ExportBehaviour;
+
+namespace GitHydra.Tests
+{
+    public class SprintTest
+    {
+        private readonly StringWriter _writer = new();
+
+        public SprintTest()
+        {
+            Console.SetOut(_writer);
+        }
+
+        [Fact]
+        public void Export_Sprint_To_PDF()
+        {
+            var exportMethod = new ExportPDF();
+            var sprint = new ReleaseSprint("Login feature", DateTime.Now, DateTime.Now, new ScrumMaster("Mark", "Mark@gmail.com"), exportMethod);
+
+            sprint.Export();
+
+            Assert.Equal("Exporting sprint Login feature to PDF...", _writer.ToString().Trim());
+        }
+
+        [Fact]
+        public void Export_Sprint_To_PNG()
+        {
+            var exportMethod = new ExportPNG();
+            var sprint = new ReleaseSprint("Login feature", DateTime.Now, DateTime.Now, new ScrumMaster("Mark", "Mark@gmail.com"), exportMethod);
+
+            sprint.Export();
+
+            Assert.Equal("Exporting sprint Login feature to PNG...", _writer.ToString().Trim());
+        }
+    }
+}
